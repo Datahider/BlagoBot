5b17e42b13ce22930355ccd0a182eb03efc72338
web <web@10.0.2.15>
2025-05-05 15:09:59 +0300

    Убрал из периода 2024 год и добавил 2029

--- src/params/ParamDescriptionPeriod.php
+++ src/params/ParamDescriptionPeriod.php
@@ -6,11 +6,11 @@ class ParamDescriptionPeriod extends AbstractParamDescription {
     
     protected function initValueSetAndDefauls() {
         $this->value_set = [
-            new ParamValue(2024),
             new ParamValue(2025),
             new ParamValue(2026),
             new ParamValue(2027),
             new ParamValue(2028),
+            new ParamValue(2029),
         ];
         $this->defaults = [];
     }
